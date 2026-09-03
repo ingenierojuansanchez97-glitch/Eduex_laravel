8b97d104f184dd1c8448f2341c5f768177faaaf8
ingenierojuansanchez97-glitch <ingenierojuansanchez97@gmail.com>
2026-09-03 11:37:15 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+set -e
+
+# Correr migraciones y limpiar cachés
 php artisan migrate --force
-php artisan config:clear
-php artisan cache:clear
-exec apache2-foreground
+php artisan config:cache
+php artisan route:cache
+
+# Arrancar el servidor integrado de PHP usando la variable PORT de Render
+exec php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
